9eaf68a84d79b071ca0cde46850896af0cb77e58
弥永大 <masaru@yanagadainoMacBook-Pro.local>
2026-04-05 17:32:58 +0900

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>テスト</title>
+    <title>掲示板トップ</title>
+
+    <style>
+        body {
+            background: #f5f5f5;
+            text-align: center;
+            margin-top: 100px;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            background: #333;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
+
 </head>
 <body>
-    <h1>俺のLaravel</h1>
-    <p>ここまで来た</p>
+
+    <h1>掲示板アプリ</h1>
+
+    <p>一覧ページはこちら</p>
+
+    <a href="/list" class="btn">掲示板を見る</a>
+
 </body>
 </html>
